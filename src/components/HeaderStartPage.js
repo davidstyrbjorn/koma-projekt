509cc1f6b6ff23a85ee6b792237f471233620dde
endjui <45940015+endjui@users.noreply.github.com>
2019-11-20 15:15:56 +0100

    fucked header
    as title

--- src/components/HeaderStartPage.js
+++ src/components/HeaderStartPage.js
@@ -8,11 +8,6 @@ function HeaderStartPage(props){
 
 return(
   <div class="WrapperHeader">
-      <head>
-        <meta name="description" content="Dungeons and Dragons character creation"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-          <title>Scroll</title>
-      </head>
       <header>
         <nav class="nav_wrapper">
           <li><img class="menu_pic" src={menubutton} alt="menu button"/></li>
