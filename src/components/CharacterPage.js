b50d8f1fce0d3a94a0bc4bf75561910e9961f4d2
Emil Bertholdsson <emil.bertholdsson@gmail.com>
2019-12-09 14:34:34 +0100

    hp xp looking good

--- src/components/CharacterPage.js
+++ src/components/CharacterPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from 'react-modal';
+import close from '../../src/close.png'
 
 import '../style/CharacterPage.css'
 
@@ -278,9 +279,8 @@ function HPAndXPModal(props){
             <div className="HP-change info-change">
                 <h4>Current HP: {props.character.hp} </h4>
                 <div className="input-group">
-                    
-                    <input type="number" placeholder="Incrementation" value={incrementerHP} onChange={e => {setIncrementerHP(e.target.value)}}></input>
                     <button onClick={e => { updateHP(-1) }}>-</button>
+                    <input type="number" placeholder="Incrementation" value={incrementerHP} onChange={e => {setIncrementerHP(e.target.value)}}></input>
                     <button onClick={e => { updateHP(1) }}>+</button>
                 </div>
             </div>
@@ -354,7 +354,7 @@ function BaseStatCard(props){
                 shouldCloseOnOverlayClick={true}
                 className="Modal"
             >
-                <button className="closeModal" onClick={() => closeModal()}>X</button>
+                <button className="closeModal" onClick={() => closeModal()}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M18.3 5.71c-.39-.39-1.02-.39-1.41 0L12 10.59 7.11 5.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L10.59 12 5.7 16.89c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L12 13.41l4.89 4.89c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z"/></svg></button>
                 <h2>{props.base_stat.name}</h2>
                 {modifier >= 0 && <p>{props.base_stat.level}+{modifier}</p> }     
                 {modifier < 0 &&  <p>{props.base_stat.level} {modifier}</p>}
