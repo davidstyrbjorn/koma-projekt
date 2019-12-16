9fbf0cda0a642743f378d0dfed0f13536d6daaac
David Styrbjörn <davidstyrbjorn@gmail.com>
2019-12-16 14:10:44 +0100

    Updates

---
+++ precache-manifest.c7544ff4b9d1c84e6f8089832c05fbb1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f358de01016f355903176cb94176291a",
+    "url": "/Mieeh/koma-projekt/index.html"
+  },
+  {
+    "revision": "adcb159e479f1c291ebc",
+    "url": "/Mieeh/koma-projekt/static/css/main.b038f5b3.chunk.css"
+  },
+  {
+    "revision": "710df251eba923d9f42d",
+    "url": "/Mieeh/koma-projekt/static/js/2.36c755f4.chunk.js"
+  },
+  {
+    "revision": "adcb159e479f1c291ebc",
+    "url": "/Mieeh/koma-projekt/static/js/main.4e63b544.chunk.js"
+  },
+  {
+    "revision": "9d674748e3e063ba1df0",
+    "url": "/Mieeh/koma-projekt/static/js/runtime-main.6f642108.js"
+  },
+  {
+    "revision": "bd5c97b5cdab34698200395bb4a044d1",
+    "url": "/Mieeh/koma-projekt/static/media/Intelligence_color.bd5c97b5.png"
+  },
+  {
+    "revision": "509a3f715d32beb5c656527aa6ccbaff",
+    "url": "/Mieeh/koma-projekt/static/media/Strength_color.509a3f71.png"
+  },
+  {
+    "revision": "51c18790a148a906e7c58a975529dcd9",
+    "url": "/Mieeh/koma-projekt/static/media/charisma.51c18790.png"
+  },
+  {
+    "revision": "fc06735b2928a9ea35f920bf57b01c14",
+    "url": "/Mieeh/koma-projekt/static/media/constitution_color.fc06735b.png"
+  },
+  {
+    "revision": "e0a467761f7f2bbb37da14e0797719a6",
+    "url": "/Mieeh/koma-projekt/static/media/dexterity.e0a46776.png"
+  },
+  {
+    "revision": "b2ff27aff57fcda683ad254e59a61e06",
+    "url": "/Mieeh/koma-projekt/static/media/elf.b2ff27af.png"
+  },
+  {
+    "revision": "b564c383dca31261ec39c50b2253e13a",
+    "url": "/Mieeh/koma-projekt/static/media/knight.b564c383.png"
+  },
+  {
+    "revision": "abfd03a28824109f7ccfdcf15fe6a94d",
+    "url": "/Mieeh/koma-projekt/static/media/orc.abfd03a2.png"
+  },
+  {
+    "revision": "9f34dfdd814c83dd764ca69bf9dbbd18",
+    "url": "/Mieeh/koma-projekt/static/media/rogue.9f34dfdd.png"
+  },
+  {
+    "revision": "e0680679fcf164098c299cb06ac28ca7",
+    "url": "/Mieeh/koma-projekt/static/media/wisdom_color.e0680679.png"
+  },
+  {
+    "revision": "28c76236f3af2526d8115fa1ca151548",
+    "url": "/Mieeh/koma-projekt/static/media/wizard_color.28c76236.png"
+  }
+]);
